f5878ea15a9050ed5310a5592c170b49e0d01f28
Daniel Hiller <daniel@hiller.email>
2024-04-29 18:47:46 +0200

    fix: add shebang

--- cron/patch.sh
+++ cron/patch.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 ### This file is used to patch existing systems
 ### Thats the only way to patch configs or scripts (like backup or update scripts) 
 ### It should be the most time empty
