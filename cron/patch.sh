90f111c4fcafd572ca9608a66069de72e8e77af7
Daniel Hiller <daniel@hiller.email>
2024-07-13 14:50:53 +0200

    Fix: cleanup patch

--- cron/patch.sh
+++ cron/patch.sh
@@ -2,7 +2,4 @@
 ### This file is used to patch existing systems
 ### Thats the only way to patch configs or scripts (like backup or update scripts) 
 ### It should be the most time empty
-# 29 April 2024
-DOWNLOAD_URL_CRON_FILE="https://raw.githubusercontent.com/plankanban/planka-installer/main/cron/planka-cron"
-curl -fsSL $DOWNLOAD_URL_CRON_FILE -o /etc/cron.daily/planka-cron
-chmod +x /etc/cron.daily/planka-cron
+echo "Nothing to do here"
